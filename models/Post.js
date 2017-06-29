b91971e4950d4a413c1a3fa22a194fb3696414d2
Jeremy Ross <jeremypross@gmail.com>
2017-06-28 21:34:05 -0400

    post model

---
+++ models/Post.js
@@ -0,0 +1,60 @@
+const db = require('../config/db');
+
+const Post = {};
+
+Post.findAll = () => {
+  return db.query(`SELECT * FROM posts;`);
+};
+
+Post.create = (post, id) => {
+  return db.one(`
+    INSERT INTO posts
+    (title, image_url, source_url, category, user_id)
+    VALUES
+    ($1, $2, $3 ,$4, $5) RETURNING *`,
+    [
+      post.title,
+      post.image_url,
+      post.source_url,
+      post.category,
+      id
+    ]
+  );
+};
+
+Post.findById = (id) => {
+  return db.oneOrNone(`
+    SELECT * FROM posts
+    WHERE user_id = $1`,
+    [id]
+  );
+};
+
+Post.update = (post, id) => {
+  return db.none(`
+    UPDATE posts
+    SET
+      title = $1,
+      image_url = $2,
+      source_url = $3,
+      category = $4,
+    WHERE id = $5`,
+    [
+      post.title,
+      post.image_url,
+      post.source_url,
+      post.category,
+      id
+    ]
+  );
+};
+
+Post.destroy = (id) => {
+  return db.none(`
+    DELETE FROM users
+    WHERE id = $1`,
+    [id]
+  );
+};
+
+module.exports = Post;
