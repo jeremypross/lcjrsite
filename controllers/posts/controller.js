dada3b2d56b74b60cad6f13f54fdfefb17a07cfb
Jeremy Ross <jeremypross@gmail.com>
2017-06-28 21:47:37 -0400

    posts controller

---
+++ controllers/posts/controller.js
@@ -0,0 +1,26 @@
+const Post = require('../../models/Post');
+const controller = {};
+
+controller.index = (req, res) => {
+  Post
+    .findAll()
+    .then((data) => {
+      res.json(200)
+    })
+    .catch((err) => {
+      console.log('ERROR', err);
+    })
+}
+
+controller.create = (req, res) => {
+  Post
+    .create(req.body.post)
+    .then((data) => {
+      res.sendStatus(201)
+    })
+    .catch((err) => {
+      console.log('ERROR', err);
+    })
+}
+
+module.exports = controller;
---
+++ controllers/posts/index.js
@@ -0,0 +1,8 @@
+const router     = require('express').Router();
+const controller = require('./controller');
+
+router.get('/', controller.index);
+
+router.post('/', controller.create);
+
+module.exports = router;
