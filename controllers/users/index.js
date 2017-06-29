28d507da7cba80deaa9b43f9a73d6c447d00dc2c
Jeremy Ross <jeremypross@gmail.com>
2017-06-28 21:19:40 -0400

    git ignore

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
-
 router.get('/', controller.index);
 
 router.get('/dashboard', controller.authorizeToken);
 
-// router.post('/login', controller.login);
+router.post('/login', controller.login);
 
 router.post('/signup', controller.create);
 
